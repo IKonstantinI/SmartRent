fb09c26f44b503723a35e3ed9dc4c3f9f5d5f44d
Konstantin <diel404@gmail.com>
2025-01-20 15:36:46 +0300

    Initial Commit

---
+++ SmartRent/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SmartRent
+//
+//  Created by Konstantin on 20.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
